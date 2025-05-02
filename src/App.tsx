be2848aafcf488ce4a2f4ca31e0f94b55b1672b8
Matthew Roberts <114992138+hetbet@users.noreply.github.com>
2025-05-02 20:25:35 +0100

    feat(fe): complete woo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [currentInput, setInput] = useState("");
+
+  const handleInputChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea className="MarkdownEnter" placeholder="Enter text to be marked down here..." value={currentInput} onChange={handleInputChange}></textarea>
+      
+      <div className="MarkdownOutput">
+        <ReactMarkdown children={ currentInput } />
+      </div>
     </div>
   );
 }
